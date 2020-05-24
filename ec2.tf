454d3d461f56ca8c8180112f0b506a29ff3ca156
Marcelo Telles <mstelles@gmail.com>
2020-05-24 18:44:46 +0200

    commit

--- ec2.tf
+++ ec2.tf
@@ -45,26 +45,30 @@ resource "aws_instance" "magento" {
   }
   user_data  = <<-EOF
     #! /bin/bash
-    curl -o /tmp/magento_install.sh https://raw.githubusercontent.com/mstelles/magento/master/magento_preinstall.sh
-    bash /tmp/magento_preinstall.sh
-    echo "installing magento"
-    useradd -m -c "Magento 2" magento
-    export COMPOSER_HOME="/root/.config/composer"
-    curl -q -o /tmp/auth.json https://raw.githubusercontent.com/mstelles/magento/master/auth.json
-    mkdir /root/.composer
-    cp /tmp/auth.json /root/.composer
-    cd /var/www/html
-    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
-    composer create-project --repository=https://repo.magento.com/ magento/project-community-edition magento2
-    cd /var/www/html/magento2
-    find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
-    find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
-    chown -R magento:www-data .
-    chmod u+x bin/magento
-    bin/magento setup:install --base-url=https://${aws_alb.magentoalb.dns_name}/ --db-host=localhost --db-name=magento --db-user=magento --db-password=magento --admin-firstname=admin --admin-lastname=admin --admin-email=admin@admin.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=Africa/Johannesburg --use-rewrites=1
-
+    curl -o /tmp/magento_preinstall.sh https://raw.githubusercontent.com/mstelles/magento/master/magento_preinstall.sh
+    if -f [ "$?" == "0" ]; then
+      bash -x /tmp/magento_preinstall.sh
+      echo "installing magento"
+      useradd -m -c "Magento 2" magento
+      export COMPOSER_HOME="/root/.config/composer"
+      curl -q -o /tmp/auth.json https://raw.githubusercontent.com/mstelles/magento/master/auth.json
+      mkdir /root/.composer
+      cp /tmp/auth.json /root/.composer
+      cd /var/www/html
+      curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/bin --filename=composer
+      composer create-project --repository=https://repo.magento.com/ magento/project-community-edition magento2
+      cd /var/www/html/magento2
+      find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} +
+      find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} +
+      chown -R magento:www-data .
+      chmod u+x bin/magento
+      bin/magento setup:install --base-url=https://${aws_alb.magentoalb.dns_name}/ --db-host=localhost --db-  name=magento --db-user=magento --db-password=magento --admin-firstname=admin --admin-lastname=admin --  admin-email=admin@admin.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD -  -timezone=Africa/Johannesburg --use-rewrites=1
+    else
+      echo "Couldn't download pre-install script. Exiting"
+    fi
     echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/test.html
     echo "ALB: ${aws_alb.magentoalb.dns_name}" | sudo tee -a /var/www/html/test.html
+    rm /tmp/auth.json
     EOF
   depends_on = ["aws_vpc.mainvpc", "aws_subnet.public_subnets", "aws_security_group.ec2_public_security_group"]
 }
